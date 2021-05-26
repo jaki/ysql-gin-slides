5bee84eaf2f02ca7df43a9ce28d28ef3e84abef2
Jason Kim <git@jasonk.me>
2021-05-25 18:27:38 -0700

    refactor: rm "previous: " which causes line wrap

--- src/sql/05-1.sql
+++ src/sql/05-1.sql
@@ -1,3 +1,3 @@
--- previous: INSERT INTO arrs VALUES ('i', ARRAY[1, 3, 5, 3]);
+-- INSERT INTO arrs VALUES ('i', ARRAY[1, 3, 5, 3]);
 INSERT INTO arrs VALUES ('j', ARRAY[1, 3, 2]);
 INSERT INTO arrs VALUES ('k', ARRAY[5, 3]);
